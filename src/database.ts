c1d87e72ada019fed5efeebb733d32e39dfae980
NicolasLOrtiz <contato.nicolol@gmail.com>
2025-08-20 07:50:16 -0300

    Requisitos da aplicação

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,5 @@
-import {Knex, knex} from 'knex'
 import {env} from "./env";
-
-if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL env not found.')
-}
+import knex, {Knex} from 'knex'
 
 export const config: Knex.Config = {
     client: 'sqlite',
